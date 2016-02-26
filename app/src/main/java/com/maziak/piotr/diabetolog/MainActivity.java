14d674516de27773f5deebb0239680f43bcb38d0
pietro-77 <piotr.maziak@gmail.com>
2016-02-26 14:57:44 +0100

    Update MainActivity.java
    gh

--- app/src/main/java/com/maziak/piotr/diabetolog/MainActivity.java
+++ app/src/main/java/com/maziak/piotr/diabetolog/MainActivity.java
@@ -120,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         canvas.drawLine(0, 200, 480,200, paint);
         // edytowane w as
         canvas.drawLine(0, 300, 480,300, paint);
-//gh
+//gh gh gh
         canvas.drawLine(0, 400, 480,400, paint);
         canvas.drawLine(0, 500, 480,500, paint);*/
         //edit on githu
